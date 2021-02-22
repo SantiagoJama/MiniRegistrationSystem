682401df64ed0f48edef3f9d0a246364ef166208
Santiago Jama <santiagojama@outlook.com>
2021-02-22 12:34:03 -0500

    index file fixed

--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 require_once('src/controller/Managerc.php');
 
 if (!isset($_REQUEST['c'])){
+    
     $my_controller= new Managerc();
     $my_controller->get_home();
+
 }
 else{
+
     $controller = $_REQUEST['c'];
     $action = $_REQUEST['a'];
     $my_controller = new $controller();
+
     switch ($action){
         case "Guardar":
             $my_controller->set_data();
